62e56cba971c51ccb2fb49638ff71fd7fd35fcb5
Tobias <tobi5457@edu.easj.dk>
2020-09-03 12:23:31 +0200

    Første commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <title>Sigende titel</title>
+    <?php include 'metatags.php'; ?>
+
+    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/stylesIndex.min.css" rel="stylesheet" type="text/css">
+    <link href="css/stylesUniversal.min.css" rel="stylesheet" type="text/css">
+    <link href="css/stylesHeader.min.css" rel="stylesheet" type="text/css">
+</head>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<body>
+
+<!-- Her skal sidens indhold ligge -->
+<div class="topHeader">
+    <div class="profilePicture"></div>
+    <a href="#" class="websiteURL">www.<p>GardenShop</p>.dk</a>
+</div>
+
+<header class="stickyHeader">
+    <div>
+        <i class="fas fa-tachometer-alt"></i>
+        <p>Dashboard</p>
+    </div>
+    <i class="fas fa-bars"></i>
+</header>
+
+<div class="blurBackdrop"></div>
+<section class="menu">
+
+</section class="menu">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+
+    $(function () {
+
+    });
+
+</script>
+</body>
+</html>
---
+++ metatags.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+
+
+<!-- Titel som ses oppe i browserens tab mv. -->
+
+<!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+<meta charset="utf-8">
+
+
+
+<!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+
